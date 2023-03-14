84ddd555d69e6695a178c7f03ec0fde6fb15c846
Robin Goerlach <robin.goerlach@sasd.de>
2023-03-14 11:36:02 +0100

    Empty Main function commit

---
+++ 000/001_main.c
@@ -0,0 +1,10 @@
+/* ******************************************************* *
+ * Simple Main program for C  
+ *
+ * ******************************************************* */
+
+// Header file for  basic console input output functions
+#include <stdio.h>
+
+// main function is where the execution of program begins
+int main() { return 0; }
---
+++ 000/002_main.c
@@ -0,0 +1,16 @@
+/* ******************************************************* *
+ * Simple Main program for C  
+ * The arguments argc and argv are used as a way to send 
+ * arguments to a program
+ * ******************************************************* */
+
+// Header file for  basic console input output functions
+#include <stdio.h>
+
+// main function is where the execution of program begins
+int main(int argc, char **argv)
+{
+
+	return 0;
+
+}
